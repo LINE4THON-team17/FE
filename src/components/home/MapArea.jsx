cca34bf208acc113f8141d9d67f38afc9f3ad4b2
조아 <parkjoah@naver.com>
2025-11-05 21:17:01 +0900

    feat: connect kakaoMap(#2)

---
+++ src/components/home/MapArea.jsx
@@ -0,0 +1,16 @@
+import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
+
+export const MapArea = () => {
+  useKakaoLoader();
+
+  return (
+    <div className="mx-[-112px] h-[calc(100vh-140px)]">
+      <Map
+        className="w-full h-full"
+        id="map"
+        center={{ lat: 33.450701, lng: 126.570667 }}
+        level={3}
+      />
+    </div>
+  );
+};
